6ce3fb9485d0489c6c62c58626e2587e73491071
Oleksii Volkov <lifecoder.ua@gmail.com>
2020-07-20 12:06:31 +0300

    mailgun integration

---
+++ app.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+// process.env.SOURCE
+console.log(process.env.SENDER_NAME)
+// process.env.EMAIL_PASSWORD
+// process.env.EMAIL_SUBJECT
+// process.env.EMAIL_RECIPIENT
